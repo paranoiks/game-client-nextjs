e95b8c2389b482e88f82893317f6642a1244243b
paranoiks <valo.balt@gmail.com>
2024-01-20 17:20:01 +0000

    Login support

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,36 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState } from 'react';
+import { useRouter } from 'next/router'
 
 export default function Home() {
+
+  const [username, setUsername] = useState("");
+  const [playerId, setPlayerId] = useState(0);
+
+  const router = useRouter()
+
+  const handleUsernameChange = (event) => {
+    const { name, value } = event.target;
+
+    setUsername(value);
+  };
+
+  const login = async (username) => {
+    const res = await fetch("http://localhost:3001/player/" + username, {method: "POST"});
+
+    const json = await res.json();
+    const id = json.id;
+    console.log(id);
+  };
+
+  const loginClickHandler = () => {
+    login(username);
+    router.replace('/lobby');
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,121 +40,17 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to game server!
         </h1>
+        <div className={styles.loginForm}>
+          <input id="username" type='text' height="96" className={styles.username} onChange={handleUsernameChange} placeholder='Username'></input>
+          <button onClick={loginClickHandler}>Login</button>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
+
+
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
---
+++ pages/lobby/index.js
@@ -0,0 +1,9 @@
+import styles from './Lobby.module.css';
+
+export default function Lobby() {
+    return (<div className={styles.container}>
+    <h1 className={styles.title}>
+          Welcome to the lobby!
+        </h1>
+    </div>)
+}
